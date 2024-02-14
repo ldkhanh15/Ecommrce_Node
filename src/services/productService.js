5f6e59a87c249e5986f515170b4e5dfe91bdd954
KhanhDev <ledinhkhanh2004@gmail.com>
2024-02-14 23:20:40 +0700

    update model product

--- src/services/productService.js
+++ src/services/productService.js
@@ -6,12 +6,14 @@ import cloudinary from 'cloudinary'
 const getProduct = (req) => {
     return new Promise(async (resolve, reject) => {
         try {
-            console.log(req.body.id);
             let data = !req.body.id ?
                 await db.Product.findAll({
                     include: [
                         {
-                            model: db.Shop, as: 'shop', attributes: ['name']
+                            model: db.ProductDetail, as: 'detailProduct', attributes: ['additional', 'brand', 'quantity']
+                        },
+                        {
+                            model: db.Shop, as: 'shop', attributes: ['name', 'avatar', 'address', 'phone', 'introduce']
                         },
                         {
                             model: db.Cate, as: 'cate', attributes: ['name']
@@ -65,6 +67,9 @@ const createProduct = (req) => {
         try {
             const error = joi.object({ color, combo, size, description, additional, introduce, idCate, nameProduct, price, sale, quantity }).validate(req.body)
             if (error.error) {
+                Promise.all(req.files?.map(async (file) => {
+                    await cloudinary.uploader.destroy(file.filename)
+                }))
                 resolve({
                     code: 0,
                     message: error.error?.details[0].message
@@ -76,7 +81,8 @@ const createProduct = (req) => {
                     sold: 0,
                     name: req.body.nameProduct,
                     mainImage: req.files[0].path,
-                    hoverImage: req.files[1].path
+                    hoverImage: req.files[1].path,
+                    idShop: req.user.id
                 })
                 await product.save();
                 Promise.all(req.files?.map(async (file, index) => {
@@ -121,6 +127,135 @@ const createProduct = (req) => {
             }
 
         } catch (error) {
+            Promise.all(req.files?.map(async (file) => {
+                await cloudinary.uploader.destroy(file.filename)
+            }))
+            reject(error)
+        }
+    })
+}
+
+const updateProduct = (req) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const error = joi.object({ id, color, combo, size, description, additional, introduce, idCate, nameProduct, price, sale, quantity }).validate(req.body)
+            if (error.error) {
+                Promise.all(req.files?.map(async (file) => {
+                    await cloudinary.uploader.destroy(file.filename)
+                }))
+                resolve({
+                    code: 0,
+                    message: error.error?.details[0].message
+                })
+            } else {
+                let product = await db.Product.findOne({
+                    where: {
+                        id: req.body.id
+                    }
+                })
+                if (!product) {
+                    Promise.all(req.files?.map(async (file) => {
+                        await cloudinary.uploader.destroy(file.filename)
+                    }))
+                    resolve({
+                        code: 0,
+                        message: 'Product ID not found'
+                    })
+                } else {
+                    let images = await db.ProductImage.findAll({
+                        where: {
+                            idProduct: req.body.id,
+                        },
+                        raw: true
+                    })
+                    Promise.all(
+                        images.map(async (image) => {
+                            await cloudinary.uploader.destroy(image.fileName)
+                        })
+                    )
+                    await db.ProductImage.destroy({
+                        where: {
+                            idProduct: req.body.id
+                        }
+                    })
+                    await db.ProductDetail.destroy({
+                        where: {
+                            idProduct: req.body.id
+                        }
+                    })
+                    await db.ProductSize.destroy({
+                        where: {
+                            idProduct: req.body.id
+                        }
+                    })
+                    await db.Combo.destroy({
+                        where: {
+                            idProduct: req.body.id
+                        }
+                    })
+                    await db.Color.destroy({
+                        where: {
+                            idProduct: req.body.id
+                        }
+                    })
+
+                    await db.Product.update({
+                        ...req.body,
+                        sold: 0,
+                        name: req.body.nameProduct,
+                        mainImage: req.files[0].path,
+                        hoverImage: req.files[1].path,
+                    }, {
+                        where: {
+                            id: req.body.id
+                        }
+                    })
+                    Promise.all(req.files?.map(async (file) => {
+                        let image = await db.ProductImage.create({
+                            idProduct: product.id,
+                            link: file.path,
+                            fileName: file.filename
+                        })
+                        await image.save();
+                    }))
+
+                    let productDetail = await db.ProductDetail.create({
+                        ...req.body,
+                        idProduct: product.id
+                    })
+                    await productDetail.save()
+                    req.body.size && Promise.all(req.body.size.map(async (item) => {
+                        let sizeItem = await db.ProductSize.create({
+                            idProduct: product.id,
+                            idSize: item
+                        })
+                        await sizeItem.save()
+                    }))
+                    req.body.combo && Promise.all(req.body.combo.map(async (item) => {
+                        let comboItem = await db.Combo.create({
+                            idProduct: product.id,
+                            name: item
+                        })
+                        await comboItem.save()
+                    }))
+                    req.body.color && Promise.all(req.body.color.map(async (item) => {
+                        let colorItem = await db.Color.create({
+                            idProduct: product.id,
+                            name: item
+                        })
+                        await colorItem.save()
+                    }))
+                    resolve({
+                        message: 'Successfully',
+                        code: 1
+                    })
+                }
+            }
+
+        } catch (error) {
+            Promise.all(req.files?.map(async (file) => {
+                await cloudinary.uploader.destroy(file.filename)
+            }))
             reject(error)
         }
     })
@@ -148,7 +283,6 @@ const deleteProduct = (req) => {
                 } else {
                     let images = await db.ProductImage.findAll({
                         where: { idProduct: req.body.id },
-                        raw: true
                     })
                     Promise.all(images.map(async (image) => {
                         await cloudinary.uploader.destroy(image.fileName)
@@ -209,4 +343,5 @@ module.exports = {
     getProduct,
     createProduct,
     deleteProduct,
+    updateProduct
 }
